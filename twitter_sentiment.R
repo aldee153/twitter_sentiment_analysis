bf5ba23c005ca71a31eda1bc37648c38a7a7d9be
Allan <allanbutler9@gmail.com>
2017-12-27 09:15:35 -0600

    Updated 12/27/2017

---
+++ twitter_sentiment.R
@@ -0,0 +1,193 @@
+
+library(ggplot2)
+theme_set(theme_bw())
+library(twitteR)
+library(purrr)
+library(dplyr)
+library(stringr)
+
+api_key <- "vZ9KMcnKMlMHL4shcgeW6HUEX"
+api_secret <- "1LWtqeDXE1bZu6WOg9dL2jnC3uhRvmJIXBePyBQVvdqPfpHnUJ"
+access_token <- "2203710336-zVEVjLQVQEzkOAm2sVtCpkpNWRupfbhTiIjIbkA"
+access_token_secret <- "SyXSmu8oq7v5VvvkJYwu7reEbuS8wztXhMw9clsjUqHuJ"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+tweets <- searchTwitter("Paterno", n=5000) %>%
+  map_df(as.data.frame)
+
+# Grab only first for each user and ignore retweets
+
+tweets <- tweets %>%
+  filter(!str_detect(text, "^RT")) %>%
+  arrange(created) %>%
+  distinct(screenName, .keep_all = TRUE)
+
+
+
+library(tidyr)
+
+# Extract iPhone from Android Users
+tweets <- tweets %>% 
+  select(id, statusSource, text, created) %>%
+  extract(statusSource, "source", "Twitter for (.*?)<") %>%
+  filter(source %in% c("iPhone", "Android"))
+
+
+## Time of Day for Tweets per Phone
+
+
+library(lubridate)
+library(scales)
+
+tweets %>% 
+  count(source, hour = hour(with_tz(created, "EST"))) %>%
+  mutate(percent = n/sum(n)) %>%
+  ggplot(aes(hour, percent, color = source)) +
+  geom_line() +
+  scale_y_continuous(labels = percent_format()) +
+  labs(x = "Hour of day (EST)",
+       y = "% of tweets",
+       color = "")
+
+
+## Difference in Sharing Pictures in tweets
+
+
+tweet_picture_counts <- tweets %>%
+  filter(!str_detect(text, '^"')) %>%
+  count(source, 
+        picture = ifelse(str_detect(text, "t.co"),
+                         "Picture/link", "No picture/link"))
+
+ggplot(tweet_picture_counts, aes(source, n, fill=picture)) +
+  geom_bar(stat="identity", position = "dodge") +
+  labs(x = "", y = "Number of tweets", fill = "")
+
+
+## Comparison of Words
+
+
+library(tidytext)
+
+reg <- "([^A-Za-z\\d#@']|'(?![A-Za-z\\d#@]))"
+tweet_words <- tweets %>%
+  filter(!str_detect(text, '^"')) %>%
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = reg) %>%
+  filter(!word %in% stop_words$word,
+         str_detect(word, "[a-z]"))
+
+tweet_words
+
+
+## Most Common Words?
+
+tweet_words %>%
+  count(word, sort = TRUE) %>%
+  head(20) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_bar(stat = "identity") +
+  ylab("Occurrences") +
+  coord_flip()
+
+
+## Which words most likely to be from Android and most likely from iPhone?
+
+android_iphone_ratios <- tweet_words %>%
+  count(word, source) %>%
+  filter(sum(n) >= 5) %>%
+  spread(source, n, fill = 0) %>%
+  ungroup() %>%
+  mutate_each(funs((. + 1) / sum(. + 1)), -word) %>%
+  mutate(logratio = log2(Android / iPhone)) %>%
+  arrange(desc(logratio))
+
+android_iphone_ratios %>%
+  group_by(logratio > 0) %>%
+  top_n(15, abs(logratio)) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, logratio)) %>%
+  ggplot(aes(word, logratio, fill = logratio < 0)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  ylab("Android / iPhone log ratio") +
+  scale_fill_manual(name = "", labels = c("Android", "iPhone"),
+                    values = c("red", "lightblue"))
+
+
+## Sentiment Analysis:
+
+nrc <- sentiments %>%
+  filter(lexicon == "nrc") %>%
+  dplyr::select(word, sentiment)
+
+nrc
+
+
+## Count the Number of Words in Each Category
+
+sources <- tweet_words %>%
+  group_by(source) %>%
+  mutate(total_words = n()) %>%
+  ungroup() %>%
+  distinct(id, source, total_words)
+
+by_source_sentiment <- tweet_words %>%
+  inner_join(nrc, by = "word") %>%
+  count(sentiment, id) %>%
+  ungroup() %>%
+  complete(sentiment, id, fill = list(n = 0)) %>%
+  inner_join(sources) %>%
+  group_by(source, sentiment, total_words) %>%
+  summarize(words = sum(n)) %>%
+  ungroup()
+
+head(by_source_sentiment)
+
+
+## Poisson Test to Measure the Difference
+
+library(broom)
+
+sentiment_differences <- by_source_sentiment %>%
+  group_by(sentiment) %>%
+  do(tidy(poisson.test(.$words, .$total_words)))
+
+sentiment_differences
+
+
+## Visualize with 95% Confidence Interval
+
+library(scales)
+
+sentiment_differences %>%
+  ungroup() %>%
+  mutate(sentiment = reorder(sentiment, estimate)) %>%
+  mutate_each(funs(. - 1), estimate, conf.low, conf.high) %>%
+  ggplot(aes(estimate, sentiment)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high)) +
+  scale_x_continuous(labels = percent_format()) +
+  labs(x = "% increase in Android relative to iPhone",
+       y = "Sentiment")
+
+
+## Largest Changes in Each Category
+
+android_iphone_ratios %>%
+  inner_join(nrc, by = "word") %>%
+  filter(!sentiment %in% c("positive", "negative")) %>%
+  mutate(sentiment = reorder(sentiment, -logratio),
+         word = reorder(word, -logratio)) %>%
+  group_by(sentiment) %>%
+  top_n(10, abs(logratio)) %>%
+  ungroup() %>%
+  ggplot(aes(word, logratio, fill = logratio < 0)) +
+  facet_wrap(~ sentiment, scales = "free", nrow = 2) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x = "", y = "Android / iPhone log ratio") +
+  scale_fill_manual(name = "", labels = c("Android", "iPhone"),
+                    values = c("red", "lightblue"))
+
